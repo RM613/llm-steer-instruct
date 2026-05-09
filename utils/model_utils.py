8d44dbecbb7a0c33eecfe22611ae52eaab250995
Md Afridi <afridi7488@gmail.com>
2026-05-09 22:11:36 +0530

    Refactor model loading logic in model_utils.py

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -18,7 +18,7 @@ def load_model_from_tl_name(model_name, device='cuda', cache_dir=None, hf_token=
         return model, tokenizer
 
     #loading model 
-    if True or "llama" in model_name.lower() or "gemma" in model_name.lower() or "mistral" in model_name.lower(): 
+    if "llama" in model_name.lower() or "gemma" in model_name.lower() or "mistral" in model_name.lower(): 
         hf_model = AutoModelForCausalLM.from_pretrained(hf_model_name, token=hf_token, cache_dir=cache_dir)
         model = HookedTransformer.from_pretrained(model_name=model_name, hf_model=hf_model, tokenizer=tokenizer, device=device, cache_dir=cache_dir, local_files_only=False)
     else: 
